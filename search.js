c1aa81905803527bc206325b51845b6e4b6cecdc
Krrish2401 <krrish.231@lpu.in>
2026-01-29 17:00:26 +0530

    feat/Add-search-logic

--- search.js
+++ search.js
@@ -1,34 +1,33 @@
-const container = document.getElementById('productContainer');
+let param = new URLSearchParams(window.location.search);
+// console.log(param);
+let query = param.get("q");
+// console.log(query);
+
+const container = document.getElementById('results');
+const heading = document.querySelector('h4');
 
 fetch("https://dummyjson.com/products")
     .then(response => response.json())
     .then(data => {
-        data.products.forEach(product => {
+        const filteredProducts = data.products.filter(product => 
+            product.title.toLowerCase().includes(query.toLowerCase())
+        );
+
+        filteredProducts.forEach(product => {
             const title = product.title;
             const price = product.price;
             const image = product.thumbnail;
-            
+
             const productCard = document.createElement('div');
             productCard.className = 'product-card';
-            
+
             productCard.innerHTML = `
                 <img src="${image}" alt="${title}">
                 <h3>${title}</h3>
                 <p class="price">$${price}</p>
             `;
-            
+
             container.appendChild(productCard);
         });
     })
     .catch(error => console.log('Error:', error));
-
-
-const si = document.getElementsByClassName("searchInput")[0];
-
-function getResults(){
-    const query = si.value.trim();
-    if(!query) return;
-
-    window.location.href = `search.html?q=${encodeURIComponent(query)}`;
-    si.value = "";
-}
