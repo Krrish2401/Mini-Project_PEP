db27a1ce8bcbc46db1be38577c9d6545714bc43c
Krrish2401 <krrish.231@lpu.in>
2026-01-30 21:46:01 +0530

    chore/Add pagination to main

--- script.js
+++ script.js
@@ -1,27 +1,70 @@
+let cp = 1;
+let ip = 8;
+let allProducts = [];
+
 const container = document.getElementById('productContainer');
+const pb = document.getElementById('prevBtn');
+const nb = document.getElementById('nextBtn');
 
 fetch("https://dummyjson.com/products")
     .then(response => response.json())
     .then(data => {
-        data.products.forEach(product => {
-            const title = product.title;
-            const price = product.price;
-            const image = product.thumbnail;
-            
-            const productCard = document.createElement('div');
-            productCard.className = 'product-card';
-            
-            productCard.innerHTML = `
-                <img src="${image}" alt="${title}">
-                <h3>${title}</h3>
-                <p class="price">$${price}</p>
-            `;
-            
-            container.appendChild(productCard);
-        });
+        allProducts = data.products;
+        if(allProducts.length == 0){
+            container.innerHTML = `<p>OOPS! No product found!`;
+            pb.disabled = true;
+            nb.disabled = true;
+            return;
+        }
+
+        renderPage()
     })
     .catch(error => console.log('Error:', error));
 
+
+function renderPage() {
+    container.innerHTML = '';
+
+    let start = (cp - 1) * ip;
+    let end = start + ip;
+
+    let items = allProducts.slice(start, end);
+
+    items.forEach(product => {
+        let card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML = `
+        <img src ="${product.thumbnail}">
+        <h3>${product.title}</h3>
+        <p>${product.price}<p>
+        `
+
+        container.appendChild(card);
+
+        card.addEventListener("click", () => {
+            window.location.href = `http://localhost:5500/product.html?id=${product.id}`
+        })
+    })
+    const totalPages = Math.ceil(allProducts.length / ip);
+
+    pb.disabled = cp === 1;
+    nb.disabled = cp === totalPages;
+}
+
+pb.addEventListener("click", () => {
+    if (cp > 1) {
+        cp--;
+        renderPage();
+    }
+})
+
+nb.addEventListener("click", () => {
+    const totalPages = Math.ceil(allProducts.length / ip);
+    if (cp < totalPages) {
+        cp++;
+        renderPage();
+    }
+})
 
 const si = document.getElementsByClassName("searchInput")[0];
 
